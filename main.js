874629474c00a02fe67ec26371689f33b18dd3a6
Darren Jake Ambong <144804969+Ciscowow@users.noreply.github.com>
2026-02-12 21:47:10 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+const yesBtn = document.querySelector('.btn-yes');
+const noBtn = document.querySelector('.btn-no');
+
+const title = document.querySelector('.card h1');
+const subtitle = document.querySelector('.card h2');
+
+const capyLeft = document.querySelector('.capybara-left');
+const capyRight = document.querySelector('.capybara-right');
+
+const originalTitle = title.textContent;
+const originalSubtitle = subtitle.textContent;
+
+const leftOriginal = capyLeft.src;
+const rightOriginal = capyRight.src;
+
+const noMessages = [
+  {
+    h1: 'Sure na sure kana? ',
+    h2: 'Loh 🥺'
+  },
+  {
+    h1: 'Wag na mainis ',
+    h2: 'Sige na plss 😢'
+  },
+  {
+    h1: 'Hahayaan mo lang ako mag isa ',
+    h2: 'Last chance 😭'
+  },
+];
+
+let noClickIndex = 0;
+let noClicked = false;
+
+yesBtn.addEventListener('click', () => {
+  window.location.href = 'yes.html';
+});
+
+yesBtn.addEventListener('mouseenter', () => {
+  title.textContent = originalTitle;
+  subtitle.textContent = originalSubtitle;
+
+  capyLeft.src = leftOriginal;
+  capyRight.src = rightOriginal;
+
+  noClicked = false;
+  noClickIndex = 0;
+});
+
+noBtn.addEventListener('mouseenter', () => {
+  title.textContent = 'Sure kana ba dyan';
+  subtitle.textContent = 'Bruh 🫩';
+
+  capyLeft.src = 'img/1-sad.gif';
+  capyRight.src = 'img/4-sad.gif';
+
+  capyLeft.classList.add('sad-size');
+  capyRight.classList.add('sad-size');
+});
+
+noBtn.addEventListener('mouseleave', () => {
+  if (noClicked) return;
+
+  title.textContent = originalTitle;
+  subtitle.textContent = originalSubtitle;
+
+  capyLeft.src = leftOriginal;
+  capyRight.src = rightOriginal;
+
+  capyLeft.classList.remove('sad-size');
+  capyRight.classList.remove('sad-size');
+});
+
+
+noBtn.addEventListener('click', () => {
+  const message = noMessages[noClickIndex];
+
+  title.textContent = message.h1;
+  subtitle.textContent = message.h2;
+
+  capyLeft.src = 'img/1-sad.gif';
+  capyRight.src = 'img/4-sad.gif';
+
+  noClicked = true;
+  noClickIndex = (noClickIndex + 1) % noMessages.length;
+});
